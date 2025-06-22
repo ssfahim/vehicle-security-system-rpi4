fb8f5a1dbc1b0b5d6acf3be6e84a5b3ac524e23d
Sohayib Fahim <113144576+ssfahim@users.noreply.github.com>
2025-06-22 02:59:49 -0230

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import config
+import time
+from sensor_handlers import setup_sensors, monitor_sensors
+import RPi.GPIO as GPIO
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+def main():
+    print("[INFO] Vehicle Security System Starting...")
+    setup_sensors()
+    try:
+        while True:
+            monitor_sensors()
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+
+if __name__ == "__main__":
+    main()
